1ed982f35d631f627d04975e3a91708dd83e1574
DONGWON KIM <lunacircle4@likelion.org>
2020-04-12 19:58:06 +0900

    feat: video upload시 thumbnail 저장기능 구현

--- src/app/pages/videoDetail/template.js
+++ src/app/pages/videoDetail/template.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import { getThumbnails } from 'video-metadata-thumbnails';
+import React, { useState, useEffect } from 'react';
 import Header from '../../organisms/header';
 import Player from '../../organisms/player';
 import { Layout } from 'antd';
 
 export default ({url}) => {    
+  const [video, setVideo] = useState();
+  const [thumbnail, setThumbnail] = useState();
+  
+  const post_video_request = async (data) => {
+    const response = await fetch("http://localhost:8000/videos", {
+      method: 'POST', 
+      mode: 'cors', 
+      body: data
+    });
+    return await response.json();
+  }
+
+  const post_video = async(event) => {
+    event.preventDefault();
+    let formData = new FormData();
+    formData.append('video', video);
+    formData.append('thumbnail', thumbnail, video.name + "_thumbnail.jpeg");
+    await post_video_request(formData);
+  }
+
+  const get_thumbnail = async(video) => {
+    const thumbnails = await getThumbnails(
+      video, { quality: 0.6, start: 0, end: 10 }
+    );
+    return thumbnails;
+  }
+
+  const setVideoThumbnail = (e) => {
+    get_thumbnail(e.target.files[0])
+      .then(newThumbnail => setThumbnail(newThumbnail[10].blob))
+      .then(setVideo(e.target.files[0]))
+  }
+  
   return (
     <Layout className="layout">
       <Header/>
+      <form onSubmit={post_video}>
+        <input type="file" onChange={setVideoThumbnail}/>
+        <button type="submit">hoho</button>
+      </form>
       <Player url={url}/>
     </Layout>
   )
