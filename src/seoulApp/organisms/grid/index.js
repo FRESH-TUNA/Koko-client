aaefdaa12aa795675c8980685bf28e1b0e020ccc
DONGWON KIM <lunacircle4@likelion.org>
2020-04-16 19:13:15 +0900

    feat: grid prototype;

--- src/seoulApp/organisms/grid/index.js
+++ src/seoulApp/organisms/grid/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import {
-  Row, Col, Layout
-} from 'antd';
 import './.scss'
-const { Content } = Layout;
 
-export default ({xs=24, sm=12, md=8, components}) => {
-  
-  // component wrap into antd
+export default ({components}) => {
+  // component wrap into col
   components.forEach((value, index) => {
-    components[index] = <Col xs={xs} sm={sm} md={md} className="grid-col">{value}</Col>
+    components[index] = <div className="grid-col">{value}</div>
   });
 
   return (
-    <Content>
-      <Row className="grid-row" justify="start">
+    <section class="grid-wrapper">
+      <main class="grid">
         {components}
-      </Row>
-    </Content>
+      </main>
+    </section>
   );
 }
